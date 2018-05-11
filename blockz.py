5ef53e8b07ecd223f0fdbb67ff4110aed95733f0
martin-martin <breuss.martin@gmail.com>
2018-05-11 15:49:06 +0800

    docs: add docstrings

--- blockz.py
+++ blockz.py
@@ -1,8 +1,17 @@
 import hashlib as hasher
 import datetime as date
 
+
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
+        """initializes a unique Block with the input data.
+
+        Args:
+            index (int): the Block's number
+            timestamp (datetime): datetime of Block creation
+            data (str, int, etc.): data value that this Block carries
+            previous_hash (str): hash of the previous Block
+        """
         self.index = index
         self.timestamp = timestamp
         self.data = data
@@ -10,14 +19,32 @@ class Block:
         self.hash = self.hash_block()
 
     def hash_block(self):
+        """creates a unique hash from Block's input variables.
+
+        Returns:
+            str: new unique Block hash
+        """
         sha = hasher.sha256()
         sha.update(f"{self.index}{self.timestamp}{self.data}{self.previous_hash}")
         return sha.hexdigest()
 
 def create_genesis_block():
+    """creates the Block that starts the chain without a previous hash as input.
+
+    Returns:
+        Block: the genesis Block, the one and only.
+    """
     return Block(0, date.datetime.now(), "Genesis Block", "0")
 
 def next_block(previous_block):
+    """creates the next block.
+
+    Args:
+        previous_block (Block): the previous block in the blockchain
+
+    Returns:
+        Block: new Block hashed with all the necessary spices.
+    """
     this_index = previous_block.index + 1
     this_timestamp = date.datetime.now()
     this_data = f"Block {this_index} saying hello!"
